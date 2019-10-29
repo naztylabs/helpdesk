f00fcc32010262ec23cc765f32f2cdb4f7bf86f6
tylernazifi <56701738+tylernazifi@users.noreply.github.com>
2019-10-29 09:09:13 -0700

    Update Say-RemoteText.ps1

--- Say-RemoteText.ps1
+++ Say-RemoteText.ps1
@@ -1,6 +1,8 @@
 ﻿ function Say-RemoteText {
  <#
-
+This function is mainly for Friday pranks. In its current form it requires a PSSession to function, so make sure everything lines up with your domain :)
+.Example
+	Say-RemoteText -ComputerName Domain.Computer -Credential Local.Admin -Textinput Look Dave, I can see you're really upset about this
  #>
 [CmdletBinding(
  SupportsShouldProcess = $true
@@ -29,7 +31,7 @@ param(
      process{
         Enter-PSSession -ComputerName $COMPUTERNAME -Credential $credential
         
-        function remote-text{
+        function say-text{
             param ([Parameter(Mandatory=$true, ValueFromPipeline=$true)] [string] $Text)
             [Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null   
             $object = New-Object System.Speech.Synthesis.SpeechSynthesizer
