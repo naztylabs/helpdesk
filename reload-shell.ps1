bacb4d9e177689f9411d2c0e682192deea421944
naztylabs <56701738+naztylabs@users.noreply.github.com>
2021-03-01 10:53:54 -0700

    Update reload-shell.ps1
    fixing some small formatting issues

--- reload-shell.ps1
+++ reload-shell.ps1
@@ -1,21 +1,21 @@
 function reload-shell{
 <#
-.Synopsis
-Reloads your shell. 
-.Description
-This command will kill your current shell and reopen it as an admin using the credentials specified.
-In PowerShell Core it opens a cmd window to start the process. No idea why that is.
-.Parameter Version
-Which version of PowerShell you wish, accepts either pwsh or core. 
-Pwsh being Windows PowerShell. Core being PowerShell Core
-.Parameter Credential
-The elevated account you wish to open the shell as. 
-.Example
-    PS C:\>reload-shell -credential Local.Admin
-.Example
-    PS C:\>reload-shell -credential Local.Admin -version core
-.Notes
-    Created by Tyler Nazifi. I made this because I got tired of having to reopen PowerShell and dump in my credentials every time I added a function to my PS Profile.
+    .Synopsis
+        Reloads your shell. 
+    .Description
+        This command will kill your current shell and reopen it as an admin using the credentials specified.
+        In PowerShell Core it opens a cmd window to start the process. No idea why that is.
+    .Parameter Version
+        Which version of PowerShell you wish, accepts either pwsh or core. 
+        Pwsh being Windows PowerShell. Core being PowerShell Core
+    .Parameter Credential
+        The elevated account you wish to open the shell as. 
+    .Example
+      PS C:\>reload-shell -credential Local.Admin
+    .Example
+       PS C:\>reload-shell -credential Local.Admin -version core
+    .Notes
+       Created by Nazty_Labs.
 #>
 
 [CmdletBinding()]
